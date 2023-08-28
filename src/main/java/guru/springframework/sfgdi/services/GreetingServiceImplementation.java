42c8b11725a3b1aec34a4024dea0288a4d3ce765
lukas_laptop <lukasreiche@outlook.de>
2023-08-28 13:03:20 +0200

    Created separate instances of GreetingService

--- src/main/java/guru/springframework/sfgdi/services/GreetingServiceImplementation.java
+++
@@ -1,8 +0,0 @@
-package guru.springframework.sfgdi.services;
-
-public class GreetingServiceImplementation implements GreetingService{
-    @Override
-    public String sayGreeting() {
-        return "Hello World!";
-    }
-}
